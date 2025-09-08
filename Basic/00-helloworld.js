36482de6ac5ee3d3172218e56045aff06a50e573
Thechegor <Sebasurioste@gmail.com>
2025-09-07 19:53:11 -0600

    Work with helloworld and variables

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,28 @@
+//Comentario simple
+
+/*
+Esto es
+un comentario
+en varias lineas
+*/
+
+console.log("¡Hola, JavaScript!")
+console.log('¡Hola, JavaScript!')
+console.log(`¡Hola, JavaScript!`)
+
+//console.log("¡Hola, JavaScript!")
+
+/*
+console.log("¡Hola, JavaScript!")
+console.log('¡Hola, JavaScript!')
+console.log(`¡Hola, JavaScript!`)
+*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)

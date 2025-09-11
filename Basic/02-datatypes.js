2011862dd547c0ac2b606d19fc34b540db6d14e4
Thechegor <Sebasurioste@gmail.com>
2025-09-10 18:36:51 -0600

    Add datatype.js

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,36 @@
+//Tipos de datos primitivos
+
+// Cadena de texto (string)
+let name = "Sebastian"
+let alias = "Thechegor"
+let email = `Sebaschegor@email.com`
+
+//Números (number)
+let age = 37 //Enteros 
+let height = 1.77 //Decimales
+
+//Booleanos (boolean)
+let isStudent = true
+let isTeacher = false
+
+//Indefinido (undefined)
+let undefinedValue
+console.log(undefinedValue)
+
+//Nulo (null)
+let nullValue = null
+
+//Simbolo (symbol)
+let mySymbol = Symbol("mysymbol")
+
+//Gran entero (bigint)
+let myBigInt = (12038230290907421098471200293567567567567657)
+let myBigInt2 = (12038230290907421098471200293567567567567657n)
+
+//Mostrar tipos de datos
+console.log(typeof name)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
